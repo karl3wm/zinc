d0319a1f07cf17b950267ec6306611accc07c579
karl <karl3wm@writeme.com>
2025-01-31 22:38:06 +0000

    Karl's formatting changes to tools/complete.cpp

--- tools/complete.cpp
+++ tools/complete.cpp
@@ -4,43 +4,44 @@
 #include <iostream>
 #include <sstream>
 
+using namespace std;
+using namespace zinc;
+
 int main(int argc, char **argv) {
     // Initialize the OpenAI with URL, model, and API key.
     // These values should be replaced
-    std::string_view url = "https://api.sambanova.ai";
-    std::string_view model = "Meta-Llama-3.1-405B-Instruct";
-    std::string_view key = "d8957211-24e6-426d-90cc-b267ce681e4f";
-    zinc::OpenAI client(url, model, key);
+    string_view url = "https://api.sambanova.ai";
+    string_view model = "Meta-Llama-3.1-405B-Instruct";
+    string_view key = "d8957211-24e6-426d-90cc-b267ce681e4f";
+    OpenAI client(url, model, key);
 
-    std::string prompt = "";
+    string prompt = "";
     for (int i = 1; i < argc; ++ i) {
         if (i > 1) prompt += " ";
         prompt += argv[i];
     }
 
     if (prompt.empty()) {
-        std::cerr << "No arguments provided. Consuming stdin." << std::endl;
-        std::stringstream ss;
-        ss << std::cin.rdbuf();
+        cerr << "No arguments provided. Consuming stdin." << endl;
+        stringstream ss;
+        ss << cin.rdbuf();
         prompt = ss.str();
     }
 
     Log::log(zinc::span<StringViewPair>({
-        {"role", "user"},
-        {"content", prompt},
+        {"prompt", prompt}
     }));
 
-    std::cout << prompt << std::flush;
+    cout << prompt << flush;
 
-    std::string completion;
+    string completion;
     for (auto&& part : client.complete(prompt)) {
         completion += part;
-        std::cout << part << std::flush;
+        cout << part << flush;
     }
 
     Log::log(zinc::span<StringViewPair>({
-        {"role", "assistant"},
-        {"content", completion},
+        {"completion", completion},
     }));
 
     return 0;
